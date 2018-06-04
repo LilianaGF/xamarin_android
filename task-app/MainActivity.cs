07df1d0a237c63df0e002d73ff0819a2c653725e
Liliana Gutierrez <lilianagf.750@gmail.com>
2018-06-03 22:41:14 -0500

    Ejercicio 2. Ciclio de vida.

--- task-app/MainActivity.cs
+++ task-app/MainActivity.cs
@@ -1,20 +1,54 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Util;
 
 namespace task_app
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        //                                                  //tag for Log.info() method.
+        readonly string tag = "LGF";
+        //--------------------------------------------------------------------------------------------------------------
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            Log.Debug(tag, "The onCreate() event");
         }
+        //--------------------------------------------------------------------------------------------------------------
+        protected override void OnStart()
+        {
+            base.OnStart();
+            Log.Debug(tag, "The OnStart() event");
+        }
+        //--------------------------------------------------------------------------------------------------------------
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Log.Debug(tag, "The OnResume() event");
+        }
+        //--------------------------------------------------------------------------------------------------------------
+        protected override void OnPause()
+        {
+            base.OnPause();
+            Log.Debug(tag, "The OnPause() event");
+        }
+        //--------------------------------------------------------------------------------------------------------------
+        protected override void OnStop()
+        {
+            base.OnStop();
+            Log.Debug(tag, "The OnStop() event");
+        }
+        //--------------------------------------------------------------------------------------------------------------
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            Log.Debug(tag, "The OnDestroy() event");
+        }
+        //--------------------------------------------------------------------------------------------------------------
     }
+    /*END-ACTIVITY*/
 }
 
